71fa6ec8975b954b0dfa0e566e4256e5bb9e6b89
Roman <romangof@gmail.com>
2019-11-13 00:54:26 +0100

    adds material-ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import { Container, Card } from '@material-ui/core';
 
 import Header from './components/header';
 
@@ -8,7 +9,16 @@ import './App.css';
 function App() {
     return <div className="App">
         <Header />
+
         <h1> Hello, World! </h1>
+
+        <Container style={{backgroundColor: 'green'}} maxWidth={false}>
+            <Card>
+                asd
+                <br/>
+                asd
+            </Card>
+        </Container>        
     </div>;
 }
 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App.js';
 
+import './index.css';
+
 ReactDOM.render(
     <Provider store={store}>
         <App />
